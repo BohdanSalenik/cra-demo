851cff640a736d184eaf67db973d73bba8393c7f
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-02-11 10:55:23 +0200

    My name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return ( 
-    <div className="App"> <h1>Hello world!</h1> </div> 
+    <div className="App"> <h1>Bohdan Salenik</h1> </div> 
   );
 }
 
