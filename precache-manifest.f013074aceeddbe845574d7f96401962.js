e0b1a8af826db5f9d0572f252ded768eb98775ef
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-02-11 10:46:56 +0200

    Updates

---
+++ precache-manifest.f013074aceeddbe845574d7f96401962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cfd7732b576a4a276eeb2dd40b0622",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "91ec3413cd6445f23303",
+    "url": "/cra-demo/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "671757cf77c5959b7cd4",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ec3413cd6445f23303",
+    "url": "/cra-demo/static/js/main.e42664cf.chunk.js"
+  },
+  {
+    "revision": "107a7a11fab40b66012c",
+    "url": "/cra-demo/static/js/runtime-main.b7e94f1e.js"
+  }
+]);
